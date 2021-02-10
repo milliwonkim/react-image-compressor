70c692d2c3eba6a3a1c56b1f351f4edb368bd606
milliwonkim <rldnjs9347@gmail.com>
2021-02-10 13:56:13 +0900

    [add] image compression

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageCompressor from './ImageCompressor';
+
+function App() {
+  return (
+    <div className="App">
+        <ImageCompressor />
+    </div>
+  );
+}
+
+export default App;
